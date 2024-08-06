584e01dba8cfa7c20860b1ccb8c8312de9a7a08e
Esteban-02 <OESTEBAN02@GMAIL.COM>
2024-08-06 14:49:54 -0500

    Complemento de ventana y funciones

--- src/main/java/gm/tareas/controlador/IndexControlador.java
+++ src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -36,21 +36,23 @@ public class IndexControlador implements Initializable {
     private TableColumn<Tarea, String> responsableColumna;
     @FXML
     private TableColumn<Tarea, String> statusColumna;
+    @FXML
+    private TextField nombreTareaTexto;
+    @FXML
+    private TextField responsableTexto;
+    @FXML
+    private TextField estatusTexto;
+
+
+
 
     // mostrar la informacion segun como se vaya recuperando de la base de datos, se hara automaticamente
     private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
-
+    private Integer idTareaInterno;
 
     /**
      * Inicializa los diferentes metodos que se estaran usando durante toda la ejecucion del programa,
      * desde seleccionar un registro individual en la tabla, llamar los diferentes metodos de inicializacion
-     * @param url
-     * The location used to resolve relative paths for the root object, or
-     * {@code null} if the location is not known.
-     *
-     * @param resourceBundle
-     * The resources used to localize the root object, or {@code null} if
-     * the root object was not localized.
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -60,6 +62,9 @@ public class IndexControlador implements Initializable {
 
     }
 
+    /**
+     * Lista las tareas que estan en la base de datos en cada una de las filas
+     */
     private void listarTareas() {
         logger.info("Ejecutando la lista de tareas");
         tareaList.clear();
@@ -67,10 +72,120 @@ public class IndexControlador implements Initializable {
         tareaTabla.setItems(tareaList); //Se agrega todo a la tabla
     }
 
+    /**
+     * Configura los valores que se guardan en los atributos y los guarda en las columnas que estan definidas en la tabla
+     */
     private void configurarColumnas() {
         idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
         tareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
         responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
         statusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
+    }
+
+    /**
+     *  Agrega nuevas tareas a la base de datos, siguiendo unas validaciones, ademas de mostrar mensajes de alerta
+     *  segun la cituacion como si tiene un nombre o no la nueva tarea
+     */
+    public void agregarTarea(){
+        if (nombreTareaTexto.getText()==null){
+            mostrarMensaje("Error de validacion", "Debe proporcionar una tarea");
+            nombreTareaTexto.requestFocus();
+            return;
+        }else{
+            Tarea tarea = new Tarea();
+            recolectarDatosFormulario(tarea);
+            tarea.setIdTarea(null);
+            tareaServicio.guardarTarea(tarea);
+            mostrarMensaje("Informacion","Tarea agregada");
+            limpiarFormulario();
+            listarTareas();
+        }
+    }
+
+    /**
+     * Al momento de seleccionar una fila en la tabla se carga en los campos de texto
+     */
+    public void cargarTareaFormulario(){
+        var tarea = tareaTabla.getSelectionModel().getSelectedItem();     // Se selecciona una sola fila
+        if (tarea != null){
+            idTareaInterno = tarea.getIdTarea();
+            nombreTareaTexto.setText(tarea.getNombreTarea());
+            responsableTexto.setText(tarea.getResponsable());
+            estatusTexto.setText(tarea.getEstatus());
+        }
+
+    }
+
+    /**
+     * una vez que se carga un registro de la tabla queda listo para realizar la modificacion en la base de
+     * datos y realizar un <b>UPDATE</b>
+     */
+    public void modificarTarea(){
+
+        if (idTareaInterno == null){
+            mostrarMensaje("Informacion", "Seleccione una tarea");
+            return;
+        }
+        if (nombreTareaTexto.getText().isEmpty()){
+            mostrarMensaje("Informacion","Ingrese una tarea");
+            return;
+        }
+
+        Tarea tarea = new Tarea();
+        recolectarDatosFormulario(tarea);
+        tareaServicio.guardarTarea(tarea);
+        limpiarFormulario();
+        listarTareas();
+
+    }
+
+    /**
+     * Una vez agregada la nueva tarea todos los campos de los TextField son limpiados para agregar nuevas tareas
+     */
+    public void limpiarFormulario() {
+        idTareaInterno= null;
+        nombreTareaTexto.clear();
+        responsableTexto.clear();
+        estatusTexto.clear();
+    }
+
+    /**
+     * De los datos que el usuario escribe en los espacios de texto en la ventana, los recupera y los guarda en el
+     * objeto tarea para luego ser guardados en la base de datos
+     * @param tarea
+     */
+    private void recolectarDatosFormulario(Tarea tarea) {
+        if (idTareaInterno != null){
+            tarea.setIdTarea(idTareaInterno);
+        }
+        tarea.setNombreTarea(nombreTareaTexto.getText());
+        tarea.setResponsable(responsableTexto.getText());
+        tarea.setEstatus(estatusTexto.getText());
+    }
+
+    /**
+     * Metodo que mostrara mensajes en forma de alerta para mostrar informacion relevante
+     * @param titulo    Muestra el titulo de la informacion que se va a mostrar
+     * @param mensaje   Muestra el mensaje de informacion
+     */
+    private void mostrarMensaje(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public void eliminarTarea(){
+        if (idTareaInterno==null){
+            mostrarMensaje("Error", "No se ha seleccionado ninguna tarea");
+            return;
+        }
+        Tarea tarea = new Tarea();
+        recolectarDatosFormulario(tarea);
+        tareaServicio.eliminarTarea(tarea);
+        mostrarMensaje("Informacion", "Tarea eliminada");
+        limpiarFormulario();
+        listarTareas();
     }
 }
